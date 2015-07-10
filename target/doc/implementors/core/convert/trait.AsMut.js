bf0bd3dfb15a7a98ea652efaf34703a3b99a2a9f
Andrea Canciani <ranma42@gmail.com>
2015-07-10 18:28:10 +0200

    Documentation for version 0.1.0

---
+++ target/doc/implementors/core/convert/trait.AsMut.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['mmap'] = ["impl&lt;T&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/convert/trait.AsMut.html' title='core::convert::AsMut'>AsMut</a>&lt;<a href='http://doc.rust-lang.org/nightly/std/primitive.slice.html'>[T]</a>&gt; for <a class='struct' href='mmap/struct.MutMemoryMap.html' title='mmap::MutMemoryMap'>MutMemoryMap</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
